9b50fc50c0cf21730246bef613e0ad289f1c8a7a
Tobias Sibera <discjoggy@gmx.de>
2020-10-05 01:02:07 +0200

    Remove duplicated code

--- SI.UnitOfWork/UnitOfWork.cs
+++ SI.UnitOfWork/UnitOfWork.cs
@@ -1,63 +1,13 @@
 ﻿using SI.UnitOfWork.Interfaces;
 using System;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace SI.UnitOfWork
 {
-    public class UnitOfWork : IUnitOfWork
+    public class UnitOfWork : UnitOfWork<IDbContext>, IUnitOfWork
     {
-        private readonly IServiceProvider serviceProvider;
-        private bool disposed;
-
         public UnitOfWork(IDbContext dbContext, IServiceProvider serviceProvider)
+            : base(dbContext, serviceProvider)
         {
-            DbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
-            this.serviceProvider = serviceProvider ?? throw new ArgumentNullException(nameof(serviceProvider));
-        }
-
-        public IDbContext DbContext { get; }
-
-        public IRepository<TEntity> GetRepository<TEntity>()
-            where TEntity : class
-        {
-            var repository = serviceProvider.GetService(typeof(IRepository<TEntity>));
-            return (IRepository<TEntity>)repository;
-        }
-
-        public TRepository GetRepository<TEntity, TRepository>()
-            where TEntity : class
-            where TRepository : IRepository<TEntity>
-        {
-            var repository = serviceProvider.GetService(typeof(TRepository));
-            return (TRepository)repository;
-        }
-
-        public int SaveChanges() =>
-            DbContext.SaveChanges();
-
-        public Task<int> SaveChangesAsync(CancellationToken ct = default) =>
-            DbContext.SaveChangesAsync(ct);
-
-        public void Dispose()
-        {
-            Dispose(true);
-            GC.SuppressFinalize(this);
-        }
-
-        protected virtual void Dispose(bool disposing)
-        {
-            if (disposed)
-            {
-                return;
-            }
-
-            if (disposing)
-            {
-                DbContext.Dispose();
-            }
-
-            disposed = true;
         }
     }
 }
