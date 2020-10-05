48846162f0399e9d65cd067d274f69071081f0c5
Tobias Sibera <discjoggy@gmx.de>
2020-10-05 23:52:44 +0200

    Add GuidExtensions helper class

---
+++ SI.UnitOfWork.Tests/GuidExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SI.UnitOfWork.Tests
+{
+    public static class GuidExtensions
+    {
+        public static Guid ToGuid(this int value)
+        {
+            byte[] bytes = new byte[16];
+            BitConverter.GetBytes(value).CopyTo(bytes, 0);
+            return new Guid(bytes);
+        }
+
+        public static int ToInt(this Guid value)
+        {
+            byte[] bytes = value.ToByteArray();
+            int bint = BitConverter.ToInt32(bytes, 0);
+            return bint;
+        }
+    }
+}
